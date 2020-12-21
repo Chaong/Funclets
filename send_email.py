bb192c0bae5c4f135c83162622624c4acc33845d
Chaong <chaongs@qq.com>
2020-12-21 10:31:40 +0800

    Update email HTML

--- send_email.py
+++ send_email.py
@@ -18,16 +18,17 @@ def send_email(receiver, ecode):
     参数 - receiver : 收件人邮箱地址
     参数 - ecode : 验证码
     """
-    # HTML Message
-    title = ""
-    greet_one = ""
-    greet_two = ""
-    greet_three = ""
-    remark_one = ""
-    remark_two = ""
-    remark_three = ""
-    remark_four = ""
-    domain = ""
+
+    # 邮件内容
+    user = receiver.split("@")[0]   # 获得邮箱昵称
+    title = "蒲公英"
+    greet = f"嘿 ~ 亲爱的 {user}"
+    direction = "下面的蓝色代码是你在蒲公英网所获取的验证码："
+    remark_one = "如果你并未在蒲公英网获取过该验证码"
+    remark_two = "可能是因为其他用户注册或找回密码误输你的邮箱而使你收到了这封邮件"
+    remark_three = "可以放心忽略此邮件，无需进行任何操作"
+    remark_four = "若有任何问题，联系我们，将尽快为你解答"
+    domain = "puonin.com"
     content = "<table cellspacing='0' style='margin: 0 auto;'>" \
               "<tr>" \
               "<td style='padding: 30px; background-color: #007bff; border-top-left-radius: 0.25rem; border-top-right-radius: 0.25rem;'>" \
@@ -36,8 +37,8 @@ def send_email(receiver, ecode):
               "</tr>" \
               "<tr>" \
               "<td style='padding: 30px; border: 1px solid #f0f2f7; border-top: none; border-bottom-left-radius: 0.25rem; border-bottom-right-radius: 0.25rem;'>" \
-              f"<p style='margin: 0;'>{greet_one}</p>" \
-              f"<p>{greet_two}<span style='color: #007bff;'>{receiver}</span>{greet_three}</p>" \
+              f"<p style='margin: 0;'>{greet}</p>" \
+              f"<p>{direction}</p>" \
               f"<h1 style='color: #007bff; margin-top: 100px; margin-bottom: 100px; text-align: center;'>{ecode}</h1>" \
               "<hr style='border: 1px dashed #c5c5c5; margin-bottom: 30px;'/>" \
               f"<p style='color: #c5c5c5;'>{remark_one}</p>" \
@@ -48,11 +49,14 @@ def send_email(receiver, ecode):
               "</td>" \
               "</tr>" \
               "</table>"
+
+    # 定义MIMEText
     mail = MIMEText(content, "html", "utf-8")   # 实例化邮件对象，并指定格式和编码
     mail["Subject"] = Header(EMAIL_TITLE, "utf-8")   # 邮件标题以及编码
     mail["From"] = formataddr(pair=(SENDER_NAME, SENDER_EMAIL))   # 发件人
     mail["To"] = receiver   # 收件人
 
+    # 定义SMTP_SSL
     smtp = SMTP_SSL(SMTP)   # 接收邮件服务器
     smtp.login(user=SENDER_EMAIL, password=PASSWORD)   # 发送者邮件和授权码
     smtp.sendmail(SENDER_EMAIL, receiver, str(mail))   # 发送者邮件、接收者邮件、邮件对象
@@ -61,4 +65,4 @@ def send_email(receiver, ecode):
 
 if __name__ == "__main__":
     
-    send_email("", "")
+    send_email("", "")   # 收件人邮箱、验证码
