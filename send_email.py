27260eb8eb88bd56bb6856452a60ba6af2ba993f
Chaong <chaongs@qq.com>
2020-12-23 14:56:37 +0800

    Update email text

--- send_email.py
+++ send_email.py
@@ -4,11 +4,11 @@ from email.header import Header
 from email.utils import formataddr
 
 
-EMAIL_TITLE = ""   # 邮件标题
-SENDER_NAME = ""   # 发送者姓名
-SENDER_EMAIL = ""   # 发送者邮箱
+EMAIL_TITLE = "蒲公英的验证码"   # 邮件标题
+SENDER_NAME = "蒲公英"   # 发送者姓名
+SENDER_EMAIL = "hi@puonin.com"   # 发送者邮箱
 
-SMTP = ""   # 发送邮件服务器
+SMTP = "smtp.exmail.qq.com"   # 发送邮件服务器
 PASSWORD = ""   # 授权码
 
 
@@ -22,12 +22,12 @@ def send_email(receiver, ecode):
     # 邮件内容
     user = receiver.split("@")[0]   # 获得邮箱昵称
     title = "蒲公英"
-    greet = f"嘿 ~ 亲爱的 {user}"
-    direction = "下面的蓝色代码是你在蒲公英网所获取的验证码："
-    remark_one = "如果你并未在蒲公英网获取过该验证码"
-    remark_two = "可能是因为其他用户注册或找回密码误输你的邮箱而使你收到了这封邮件"
-    remark_three = "可以放心忽略此邮件，无需进行任何操作"
-    remark_four = "若有任何问题，联系我们，将尽快为你解答"
+    greet_1 = f"嘿，亲爱的 {user}"
+    greet_2 = "下面的神秘蓝色代码是你在蒲公英网所获取的验证码："
+    prompt_1 = "蒲公英的小提示：如果你并未在蒲公英网获取过该验证码，"
+    prompt_2 = "也许是其他用户注册或找回密码，误输你的邮箱而使你收到了这封邮件。"
+    prompt_3 = "你可以放心忽略此邮件。当然，你也可以选择登录蒲公英网修改密码。"
+    prompt_4 = "最后，若有任何问题，可以联系我们，我们将尽快为你解答 ~"
     domain = "puonin.com"
     content = "<table cellspacing='0' style='margin: 0 auto;'>" \
               "<tr>" \
@@ -37,14 +37,14 @@ def send_email(receiver, ecode):
               "</tr>" \
               "<tr>" \
               "<td style='padding: 30px; border: 1px solid #f0f2f7; border-top: none; border-bottom-left-radius: 0.25rem; border-bottom-right-radius: 0.25rem;'>" \
-              f"<p style='margin: 0;'>{greet}</p>" \
-              f"<p>{direction}</p>" \
+              f"<p style='margin: 0;'>{greet_1}</p>" \
+              f"<p>{greet_2}</p>" \
               f"<h1 style='color: #007bff; margin-top: 100px; margin-bottom: 100px; text-align: center;'>{ecode}</h1>" \
               "<hr style='border: 1px dashed #c5c5c5; margin-bottom: 30px;'/>" \
-              f"<p style='color: #c5c5c5;'>{remark_one}</p>" \
-              f"<p style='color: #c5c5c5;'>{remark_two}</p>" \
-              f"<p style='color: #c5c5c5;'>{remark_three}</p>" \
-              f"<p style='color: #c5c5c5;'>{remark_four}</p>" \
+              f"<p style='color: #c5c5c5;'>{prompt_1}</p>" \
+              f"<p style='color: #c5c5c5;'>{prompt_2}</p>" \
+              f"<p style='color: #c5c5c5;'>{prompt_3}</p>" \
+              f"<p style='color: #c5c5c5;'>{prompt_4}</p>" \
               f"<p style='margin: 0; color: #c5c5c5; text-align: right;'>{domain}</p>" \
               "</td>" \
               "</tr>" \
@@ -65,4 +65,4 @@ def send_email(receiver, ecode):
 
 if __name__ == "__main__":
     
-    send_email("", "")   # 收件人邮箱、验证码
+    send_email("", "Puonin")   # 收件人邮箱、验证码
