cfafbb5b415d683a079b389f12e2e69e21a2e1bd
Chaong <chaongs@qq.com>
2020-12-30 16:42:21 +0800

    Update HTML

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ from email.utils import formataddr
 
 EMAIL_TITLE = "蒲公英的验证码"   # 邮件标题
 SENDER_NAME = "蒲公英"   # 发送者姓名
-SENDER_EMAIL = "hi@puonin.com"   # 发送者邮箱
+SENDER_EMAIL = ""   # 发送者邮箱
 
 SMTP = "smtp.exmail.qq.com"   # 发送邮件服务器
 PASSWORD = ""   # 授权码
@@ -23,29 +23,35 @@ def send_email(receiver, ecode):
     user = receiver.split("@")[0]   # 获得邮箱昵称
     title = "蒲公英"
     greet_1 = f"嘿，亲爱的 {user}"
-    greet_2 = "下面的神秘蓝色代码是你在蒲公英网所获取的验证码："
-    prompt_1 = "蒲公英的小提示：如果你并未在蒲公英网获取过该验证码，"
-    prompt_2 = "也许是其他用户注册或找回密码，误输你的邮箱而使你收到了这封邮件。"
-    prompt_3 = "你可以放心忽略此邮件。当然，你也可以选择登录蒲公英网修改密码。"
-    prompt_4 = "最后，若有任何问题，可以联系我们，我们将尽快为你解答 ~"
+    greet_2 = "下面的神秘代码是你在蒲公英网所获取的验证码："
+    prompt_1 = "小提示：如果你并没有在蒲公英网获取过该验证码，"
+    prompt_2 = "也许是其他用户在注册账号或者为账号找回密码时，"
+    prompt_3 = "不小心使用你的邮箱账号而使你收到了这一封邮件。"
+    prompt_4 = "那么你可以放心忽略此邮件，当然也可以"
+    contact_email = "gy@puonin.com"
+    contact_text = "联系我们。"
     domain = "puonin.com"
     content = "<table cellspacing='0' style='margin: 0 auto;'>" \
               "<tr>" \
               "<td style='padding: 30px; background-color: #007bff; border-top-left-radius: 0.25rem; border-top-right-radius: 0.25rem;'>" \
-              f"<h2 style='margin: 0; color: #ffffff;'>{title}</h2>" \
+              "<h2 style='margin: 0;'>" \
+              f"<a href='http://puonin.com/' title='进入蒲公英' style='color: #ffffff; text-decoration: none;'>{title}</a>" \
+              "</h2>" \
               "</td>" \
               "</tr>" \
               "<tr>" \
               "<td style='padding: 30px; border: 1px solid #f0f2f7; border-top: none; border-bottom-left-radius: 0.25rem; border-bottom-right-radius: 0.25rem;'>" \
               f"<p style='margin: 0;'>{greet_1}</p>" \
               f"<p>{greet_2}</p>" \
-              f"<h1 style='color: #007bff; margin-top: 100px; margin-bottom: 100px; text-align: center;'>{ecode}</h1>" \
+              f"<h1 title='验证码' style='color: #007bff; margin-top: 100px; margin-bottom: 100px; text-align: center;'>{ecode}</h1>" \
               "<hr style='border: 1px dashed #c5c5c5; margin-bottom: 30px;'/>" \
-              f"<p style='color: #c5c5c5;'>{prompt_1}</p>" \
-              f"<p style='color: #c5c5c5;'>{prompt_2}</p>" \
-              f"<p style='color: #c5c5c5;'>{prompt_3}</p>" \
-              f"<p style='color: #c5c5c5;'>{prompt_4}</p>" \
-              f"<p style='margin: 0; color: #c5c5c5; text-align: right;'>{domain}</p>" \
+              f"<p>{prompt_1}</p>" \
+              f"<p>{prompt_2}</p>" \
+              f"<p>{prompt_3}</p>" \
+              f"<p>{prompt_4}<a href='mailto:{contact_email}' title='联系蒲公英' style='color: #000000; text-decoration: none;'>{contact_text}</a></p>" \
+              "<p style='margin: 0; text-align: right;'>" \
+              f"<a href='http://puonin.com/' title='进入蒲公英' style='color: #000000; text-decoration: none;'>{domain}</a>" \
+              "</p>" \
               "</td>" \
               "</tr>" \
               "</table>"
@@ -65,4 +71,4 @@ def send_email(receiver, ecode):
 
 if __name__ == "__main__":
     
-    send_email("", "Puonin")   # 收件人邮箱、验证码
+    send_email("", "pUoNin")   # 收件人邮箱、验证码
